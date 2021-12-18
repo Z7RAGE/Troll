d4dfa77a3a8ab403286170da87d8cac3c93179bc
Z7RAGE <72812927+Z7RAGE@users.noreply.github.com>
2021-12-18 22:56:59 +0000

    Finally adding Web files

---
+++ Web Files/Definiently Not JavaScript.js
@@ -0,0 +1,14 @@
+//No JavaScript Just a text file 100% 
+
+document.addEventListener('keydown', logkey);
+
+function logkey(e) 
+{
+    if (e.code == "F2")
+    {
+        console.log("F2 pressed");
+        window.open('https://youtu.be/dQw4w9WgXcQ');
+    }
+
+} 
+  
